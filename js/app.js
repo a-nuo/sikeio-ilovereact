d83ef7c6935a0fe102108c30f7ada883e1e58f66
a_nuo <gaoluping1985@163.com>
2015-09-28 22:32:15 +0800

    Lession6 scrollmagic

--- js/app.js
+++ js/app.js
@@ -18,11 +18,11 @@ function updateSliderControl(){
 		var link =links[i];
 
 		var section = document.querySelector(link.getAttribute("href"));
-		console.log(link.getAttribute("href"));
+		//console.log(link.getAttribute("href"));
 		var sectionTop = section.offsetTop;
 		var sectionButtom = sectionTop+window.innerHeight;
 
-		console.log(sectionTop+ " "+sectionButtom);
+		//console.log(sectionTop+ " "+sectionButtom);
 		if(window.scrollY >=sectionTop && window.scrollY <sectionButtom){
 			link.className = "active";
 		} else {
@@ -57,6 +57,28 @@ function addSmoothScrolling(){
 
 }
 
+//第一个场景使用scrollmagic 淡出
+function addScrollMagic(){
+	var controller = new ScrollMagic.Controller();
+
+	var sectionfirst = new ScrollMagic.Scene({triggerElement:"#native",triggerHook:'onEnter',duration:'100%' })
+					.setTween("#intro-section",{opacity:0})
+					//.addIndicators({name:"1 duration 100%"})
+					.addTo(controller);
+
+	var moveiphone = new ScrollMagic.Scene({triggerElement:"#native",triggerHook:'onEnter',duration:'100%' })
+					.setTween("#iphone-relay",{width:'50%',y:0})
+					//.addIndicators({name:"1 duration 100%"})
+					.addTo(controller);
+	var piniphone = new ScrollMagic.Scene({triggerElement:"#native",triggerHook:'onLeave',duration:'100%' })
+					.setPin("#iphone-relay")
+					.addIndicators({name:"Pin iphone "})
+					.addTo(controller);
+
+
+
+}
+
 window.onscroll = function() {
   // ...
   updateSliderControl();
@@ -66,6 +88,7 @@ window.onload = function() {
   animateLogo();
   updateSliderControl();
   addSmoothScrolling();
+  addScrollMagic();
   var t = new TimelineMax({yoyo: true, repeat: -1,ease: Power2.easeInOut});
 	t.to("#android-robot",1,{rotation:"-60deg"}).to("#android-robot",1,{rotation:"-30deg"});//.to("#android-robot",1,{rotation:"45deg"});
 };
